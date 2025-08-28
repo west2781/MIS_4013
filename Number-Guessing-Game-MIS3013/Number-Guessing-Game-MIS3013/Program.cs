e3c059060cae1adf45e5a772516df8275229f0b2
West2781 <Lucas.A.West-1@ou.edu>
2025-08-28 13:05:30 -0500

    Created random number game 1-5.

--- Number-Guessing-Game-MIS3013/Number-Guessing-Game-MIS3013/Program.cs
+++ Number-Guessing-Game-MIS3013/Number-Guessing-Game-MIS3013/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//data_type variable_name = value;
+Random r = new Random();
+
+var rando = r.Next(1, 6); // Generate a random number between 1 and 5. (Second number will not be included in range).
+
+Console.WriteLine($"Generated a random number with the value {rando}");
+
+int guess;
+int count = 0;
+
+do
+{
+    Console.WriteLine("Please guess a number between 1 and 5 <<");
+    string usersGuess = Console.ReadLine();
+    guess = Convert.ToInt32(usersGuess);
+    count++;
+
+    Console.WriteLine($"You guessed:{usersGuess}");
+    if (rando != guess)
+    {
+        if (guess < rando)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Incorrect guess, your guess was too low.");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Incorrect guess, your guess was too high.");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+    }
+} while (guess != rando);
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine($"Congratulations you guessed correctly and it only took {count} attempts!");
+
