8ff04745acb02750b7ee79c0f90a9fb77788719c
West2781 <Lucas.A.West-1@ou.edu>
2025-09-02 12:29:58 -0500

    Finished step 5 in Randm Number game.

--- Number-Guessing-Game-MIS3013/Number-Guessing-Game-MIS3013/Program.cs
+++ Number-Guessing-Game-MIS3013/Number-Guessing-Game-MIS3013/Program.cs
@@ -1,39 +1,60 @@
 ﻿//data_type variable_name = value;
 Random r = new Random();
+string answer = "";
 
-var rando = r.Next(1, 6); // Generate a random number between 1 and 5. (Second number will not be included in range).
-
-Console.WriteLine($"Generated a random number with the value {rando}");
-
-int guess;
-int count = 0;
-
-do
+for (int i = 0; i < int.MaxValue; i++)
 {
-    Console.WriteLine("Please guess a number between 1 and 5 <<");
-    string usersGuess = Console.ReadLine();
-    guess = Convert.ToInt32(usersGuess);
-    count++;
+    Console.WriteLine("Please input lower bound value <<");
+    string lowerBound = Console.ReadLine();
+    int lower = Convert.ToInt32(lowerBound);
 
-    Console.WriteLine($"You guessed:{usersGuess}");
-    if (rando != guess)
+    Console.WriteLine("Please input upper bound value <<");
+    string upperBound = Console.ReadLine();
+    int upper = Convert.ToInt32(upperBound);
+
+    var rando = r.Next(lower, upper + 1); // Generate a random number in the range given by the user. (Second number not included so add one.)
+
+    //Console.WriteLine($"Generated a random number with the value {rando}");
+
+    int guess;
+    int count = 0;
+   
+    do
     {
-        if (guess < rando)
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Incorrect guess, your guess was too low.");
-            Console.ForegroundColor = ConsoleColor.White;
-        }
-        else
-        {
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine("Incorrect guess, your guess was too high.");
-            Console.ForegroundColor = ConsoleColor.White;
-        }
+        Console.WriteLine($"Please guess a number between {lower} and {upper} <<");
+        string usersGuess = Console.ReadLine();
+        guess = Convert.ToInt32(usersGuess);
+        count++;
 
+        Console.WriteLine($"You guessed:{usersGuess}");
+        if (rando != guess)
+        {
+            if (guess < rando)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Incorrect guess, your guess was too low.");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Incorrect guess, your guess was too high.");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
+        }
+    } while (guess != rando);
+
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"Congratulations you guessed correctly and it only took {count} attempts!");
+
+
+    Console.WriteLine("Do you want to try your luck again? yes or no <<");
+    answer = Console.ReadLine().ToLower();
+
+    if (answer == "no")
+    {
+        i = int.MaxValue;
+        break;
     }
-} while (guess != rando);
-
-Console.ForegroundColor = ConsoleColor.Green;
-Console.WriteLine($"Congratulations you guessed correctly and it only took {count} attempts!");
-
+}
